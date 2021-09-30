f5a913c81b08c319d145465e6f475ed812ed45fe
Xeniya <kseniyafes7@gmail.com>
2021-09-29 21:44:58 -0400

    new changes

---
+++ flask-01-02-hello-world-app-Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,24 @@
+import re
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def head ():
+ return "Hello World!"
+
+@app.route("/second")
+def second():
+    return "This is the second page"
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of thirdpage"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f"Id of this page is {id}"
+
+if __name__=="__main__":
+     app.run(host='0.0.0.0', port=80)
+  #  app.run(debug=True)
